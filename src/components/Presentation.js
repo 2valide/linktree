2f7c52a666ff6ed9449a6459a5a33518aefcd631
Anthony Z <118206086+2valide@users.noreply.github.com>
2024-04-08 21:57:39 +0200

    first commit

---
+++ src/components/Presentation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Presentation = () => {
+    return (
+        <div className="intro">
+            <ul>
+                <li><a href="https://anthonyzhao.fr">PortFolio</a></li>
+                <li><a href="">GitHub</a></li>
+                <li><a href="">LinkdIn</a></li>
+            </ul>
+        </div>
+    );
+};
+
+export default Presentation;
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Presentation from '../components/Presentation';
+import '../styles/style.css';
+
+const Home = () => {
+    return (
+        <div>
+            <Presentation />
+
+        </div>
+    );
+};
+
+export default Home;
