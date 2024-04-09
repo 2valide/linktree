35b299b7e40f16d82f1bfa0be5b62c8168fa0215
Anthony Z <118206086+2valide@users.noreply.github.com>
2024-04-09 12:31:51 +0200

    start style presentation

--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -1,14 +1,14 @@
-import React from 'react';
-
+import React from "react";
+import "../styles/presentation.css";
 
 const Presentation = () => {
-    return (
-        <div className="intro">
-            <img src="" alt="" />
-            <h1>Anthony ZHAO</h1>
-            <p>Développeur web</p>
-        </div>
-    );
+  return (
+    <div className="intro-container">
+      <img src="./logo192.png" alt="photo de profile" />
+      <h1>Anthony ZHAO</h1>
+      <p>Développeur web</p>
+    </div>
+  );
 };
 
 export default Presentation;
