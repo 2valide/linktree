772828287545866077612a4e8bdcbdc007f264fe
Anthony Z <118206086+2valide@users.noreply.github.com>
2024-04-16 10:04:55 +0200

    latest

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,9 +10,9 @@ const Home = () => {
     return (
         <div>
             <Presentation />
-            <Lien src="main.jpg" alt="" href="https://anthonyzhao.fr" site="PortFolio"/>
-            <Lien src="main.jpg" alt="" href="https://github.com/2valide" site="GitHub"/>
-            <Lien src="main.jpg" alt="" href="https://www.linkedin.com/in/anthony-zhao-0331b6298" site="LinkedIn"/>
+            <Lien src="favicon.svg" alt="" href="https://anthonyzhao.fr" site="PortFolio"/>
+            <Lien src="github-icon.svg" alt="" href="https://github.com/2valide" site="GitHub"/>
+            <Lien src="linkedin-icon.svg" alt="" href="https://www.linkedin.com/in/anthony-zhao-0331b6298" site="LinkedIn"/>
 
         </div>
     );
